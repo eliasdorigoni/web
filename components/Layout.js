fcbc08295b6a2c835aebd55092cbdec9d9f6f9bb
Elias Dorigoni <eliasdorigoni@gmail.com>
2021-12-22 13:51:32 -0300

    Revisa nav y footer

--- components/Layout.js
+++ components/Layout.js
@@ -10,33 +10,35 @@ export default function Layout({ children }) {
         <link key="font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Code+Pro:300,400,600|Source+Sans+Pro:300,400,600,700" />
       </Head>
       <div className="bg-gray-200 min-h-screen font-sans">
-        <header className="bg-primary text-white text-center pt-2 pb-4 mb-4">
-          <h1 className="mb-2">
+        <header className="bg-primary text-white text-center pt-2 mb-4">
+          <h1 className="mb-3">
             <Link href="/">
-              <a className="text-3xl font-bold">Elías Dorigoni</a>
+              <a className="text-3xl font-bold -mb-1">Elías Dorigoni</a>
             </Link>
-            <span className="block italic opacity-80 font-light">PHP web developer - Córdoba capital</span>
+            <span className="block italic opacity-80 font-light">PHP Developer</span>
           </h1>
-          <nav className="flex justify-center text-lg">
+          <nav className="flex justify-center text-lg pb-2">
             <Link href="/">
-              <a className="flex-initial mx-2 hover:underline">Artículos</a>
+              <a className="flex-initial px-2 py-1 hover:underline">Artículos</a>
             </Link>
             <Link href="/portfolio/">
-              <a className="flex-initial mx-2 hover:underline">Portfolio</a>
+              <a className="flex-initial px-2 py-1 hover:underline">Portfolio</a>
             </Link>
             <Link href="/sobre-mi/">
-              <a className="flex-initial mx-2 hover:underline">Sobre mí</a>
+              <a className="flex-initial px-2 py-1 hover:underline">Sobre mí</a>
             </Link>
           </nav>
         </header>
+
         <div className="container mx-auto px-2">
           <main className="max-w-prose mx-auto" role="main">
             {children}
           </main>
-
         </div>
+
         <footer className="container mx-auto text-gray-700 text-center text-sm pb-6">
-          <p>Desarrollado en <a className="font-bold" href="https://nextjs.org/" target="_blank" rel="noopener">next.js</a>. Hosteado en <a className="font-bold" href="https://www.netlify.com/" target="_blank" rel="noopener">Netlify</a>.</p>
+          <p>Desarrollado en <a className="font-bold" href="https://nextjs.org/" target="_blank" rel="noopener">next.js</a>.</p>
+          <p>Hosteado en <a className="font-bold" href="https://www.netlify.com/" target="_blank" rel="noopener">Netlify</a>.</p>
         </footer>
       </div>
       <NextScript />
