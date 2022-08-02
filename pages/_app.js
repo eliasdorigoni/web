d5057b6ab6c175163003baa70a95c2a9f0bf1b8e
Elias Dorigoni <eliasdorigoni@gmail.com>
2022-08-02 15:42:47 -0300

    Fixes for NextJS.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import 'tailwindcss/tailwind.css'
 import '~/source/css/styles.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,6 @@ export default function Document() {
     <Html>
       <Head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="theme-color" content="#D1382E" />
         <meta name="HandheldFriendly" content="True" />
         <meta name="robots" content="index, follow" />
